7d3fcba56ce3882dede1c938634845cf5ae1e324
Hendri <hendriwp123@gmail.com>
2025-03-09 21:43:35 +0700

    add: include new images and enhance swiper functionality with responsive breakpoints and autoplay

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,26 @@
-var swiper = new Swiper(".mySwiper", {
-	slidesPerView: 4,
-	centeredSlides: true,
+const swiper = new Swiper(".mySwiper", {
+	slidesPerView: 1,
 	spaceBetween: 30,
-	grabCursor: true,
 	pagination: {
 		el: ".swiper-pagination",
 		clickable: true,
+	},
+	breakpoints: {
+		640: {
+			slidesPerView: 1,
+			spaceBetween: 20,
+		},
+		768: {
+			slidesPerView: 2,
+			spaceBetween: 30,
+		},
+		1024: {
+			slidesPerView: 3,
+			spaceBetween: 30,
+		},
+	},
+	autoplay: {
+		delay: 3500,
+		disableOnInteraction: false,
 	},
 });
